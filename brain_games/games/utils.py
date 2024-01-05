4c36adc74f7fb0c399405b0579f505fbe3ad2f5a
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-01-05 16:30:13 +0000

    add find_gcd function to utils.py

--- brain_games/games/utils.py
+++ brain_games/games/utils.py
@@ -16,3 +16,12 @@ def generate_num():
 
 def is_even(number):
     return number % 2 == 0
+
+
+def find_gcd(number_1, number_2):
+    while number_1 != 0 and number_2 != 0:
+        if number_1 > number_2:
+            number_1 = number_1 % number_2
+        else:
+            number_2 = number_2 % number_1
+    return number_1 + number_2
