2222527903ea413d8d160519cfeac5b77892e765
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-01-11 17:53:32 +0000

    add prime number game

--- brain_games/games/utils.py
+++ brain_games/games/utils.py
@@ -1,5 +1,6 @@
 import random
 import prompt
+import math
 
 
 def get_name():
@@ -40,4 +41,15 @@ def generate_progression():
     regression[index] = '..'
 
     return (regression, missing_number)
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+
+    return True
 
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import prime_number
+
+
+def main():
+    prime_number.play_prime()
+
+
+if __name__ == '__main__':
+    main()
