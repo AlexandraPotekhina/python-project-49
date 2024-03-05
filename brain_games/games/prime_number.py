c761b5f726cf20730f7f38b8a28f7a15801d6d68
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-03-05 12:51:59 +0000

    modified evaluate func

--- brain_games/games/prime_number.py
+++ brain_games/games/prime_number.py
@@ -4,7 +4,7 @@ from brain_games.games.utils import generate_num, is_prime
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def evaluate():
-    number = generate_num
+    number = generate_num()
     question = f"Question: {number}"
     answer = 'yes' if is_prime(number) else 'no'
     return (question, answer)
